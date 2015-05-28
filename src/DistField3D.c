27e13a27d39eac00f20505906f725d8bec7f9c00
Anup Shrestha <anupshrestha@u.boisestate.edu>
2015-05-28 15:44:30 -0600

    fixed the link to the journal paper

--- src/DistField3D.c
+++ src/DistField3D.c
@@ -9,7 +9,7 @@
  * Physics titled "A parallel fast sweeping method for
  * the Eikonal equation" by Miles Detrixhe, Federic Gibou,
  * and Chohong Min.
- * DOI: http://dx.doi.org/10.1016j.jcp.2012.112042
+ * DOI: http://www.sciencedirect.com/science/article/pii/S002199911200722X
  *
  *  Created on: May 14, 2015
  *      Author: Anup Shrestha
